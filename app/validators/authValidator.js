835e8505aaa7be59ce566757403004740d189a51
Gary Yaral <gary.yaral@gmail.com>
2024-04-25 21:58:14 -0500

    auth validation properties was updated

--- app/validators/authValidator.js
+++ app/validators/authValidator.js
@@ -1,17 +1,14 @@
 const { check } = require('express-validator')
 const { customMessages } = require('../utils/customMessages.js')
-const { hardTextRegex } = require('../utils/regExp.js')
 const { validateRequest } = require('../middlewares/evaluateRequest.js')
 
 const authValidator = [
   check('username')
     .exists().withMessage(customMessages['required'])
-    .notEmpty().withMessage(customMessages['empty'])
-    .custom((value) => hardTextRegex.test(value)).withMessage(customMessages['hardText.invalid']),
+    .notEmpty().withMessage(customMessages['empty']),
   check('password')
     .exists().withMessage(customMessages['required'])
-    .notEmpty().withMessage(customMessages['empty'])
-    .custom((value) => hardTextRegex.test(value)).withMessage(customMessages['hardText.invalid']),
+    .notEmpty().withMessage(customMessages['empty']),
   async (req, res, next) => {
     validateRequest(req, res, next)
   }
